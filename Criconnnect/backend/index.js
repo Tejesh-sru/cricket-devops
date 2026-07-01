0c413a703d7e7baca0c403dc6c81968f499a11f3
Tejesh Jakkula <jakkulatejesh2005@gmail.com>
2026-07-01 22:47:59 +0530

    Fix formatting in index.js

--- Criconnnect/backend/index.js
+++ Criconnnect/backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-
+ 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo:27017/testdb';
 
